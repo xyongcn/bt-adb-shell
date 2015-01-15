892f8383064b35cd2c9ef2ff618794df55d3b2c7
wujingbang <wujingbang@gmail.com>
2015-01-15 15:44:20 +0800

    Revert "1. move files of bluetooth to Others/"
    This reverts commit 26a621d707dde698e04da31d57ed4e8bf7f2a649.

--- Others/badbd-蓝牙连接并执行shell/jni/Android.mk
+++
@@ -1,9 +0,0 @@
-LOCAL_PATH := $(call my-dir)
-include $(CLEAR_VARS)
-LOCAL_MODULE := badb-pan
-LOCAL_SRC_FILES := bluetooth_shell_proc.c
-LOCAL_CFLAGS += -I./jni/include
-LOCAL_LDLIBS += -L./jni/libfrompanda -lbluetooth
-include $(BUILD_EXECUTABLE)
-#include $(BUILD_STATIC_LIBRARY)
-#include $(BUILD_SHARED_LIBRARY)
